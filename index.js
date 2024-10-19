c2879d56395f435a323c5eee965fd04073d0f81f
govind762 <123govindchavan@gmail.com>
2024-10-20 00:31:45 +0530

    img slider

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
-const searchResults = document.getElementById('searchResults');   
+
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
 
 
-searchButton.addEventListener('click', () => {
-    const searchTerm = searchInput.value;   
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
 
-
-    // Perform search logic here (e.g., using AJAX to fetch data)
-
-    // Example: Display a simple result
-    searchResults.textContent = `Search results for "${searchTerm}":`;
-      
-    // Add your actual search results to the searchResults div
-    searchResults.style.display = 'block'; // Show the search results
+window.addEventListener("click", (e) => {
+  e.target === modal ? modal.classList.remove("show-modal") : false;
 });
+ 
